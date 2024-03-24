b1c87936478bd54cc0c5a1b05a220938b3e6729e
erapalog <erobertrapalo@hotmail.com>
2024-03-23 23:21:20 -0600

    proyecto inicial

---
+++ test-devsu/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './Components/product-list/product-list.component';
+import { AddProductComponent } from './Components/add-product/add-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    AddProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
